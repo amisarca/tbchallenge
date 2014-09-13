2d37d619f8e55e5eaaee3d0873cdb4e2f17ea026
Andrei Misarca <andrei.misarca@gmail.com>
2014-09-13 12:30:34 +0300

    Implemented the login and register pages

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,30 @@
 jQuery(function () {
   var login = function () {
-    var source = $("#login-template").html();
-    var context = { title: "Login", body: "You should login now!" }
-    $("#cntent").html(Handlebars.compile(source)(context));
+    // Change the selected menu item to Login
+    var source = $("#menu-template").html();
+    var context = { loginStatus: "active", registerStatus: "inactive" }
+    $("#menu").html(Handlebars.compile(source)(context));
+
+    // Change the text on the image
+    $("#carousel-text").text("Login");
+
+    // Display the login form
+    source = $("#login-form-template").html();
+    $("#action-form").html(Handlebars.compile(source)(context));
   };
 
   var register = function () {
-    var source = $("#login-template").html();
-    var context = { title: "Register", body: "You should register now!" }
-    $("#cntent").html(Handlebars.compile(source)(context));
+    // Change the selected menu item to Register
+    var source = $("#menu-template").html();
+    var context = { loginStatus: "inactive", registerStatus: "active" }
+    $("#menu").html(Handlebars.compile(source)(context));
+
+    // Change the text on the image
+    $("#carousel-text").text("Create your account");
+
+    // Display the register form
+    source = $("#register-form-template").html();
+    $("#action-form").html(Handlebars.compile(source)(context));
   };
 
   var routes = {
